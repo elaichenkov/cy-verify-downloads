a3633c8465eaec7921f3bcd99f003b10b2b12244
Yevhen Laichenkov <elaichenkov@gmail.com>
2022-09-14 13:38:48 +0300

    update(lib): add additional check for downloaded file

--- cypress/e2e/verify-download.cy.js
+++ cypress/e2e/verify-download.cy.js
@@ -21,4 +21,11 @@ describe('verify download functionality', () => {
 
     cy.verifyDownload('large-file-100MB', { contains: true });
   });
+
+  it('downloads file with extension and contains option', () => {
+    cy.exec(`rm -rf ${Cypress.config('downloadsFolder')}`);
+    cy.get('[data-cy="large"]').click();
+
+    cy.verifyDownload('large-file-100MB.zip', { contains: true });
+  });
 });
