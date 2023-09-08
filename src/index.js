e5c0af6379a9f074249e7965b18c8ba0f443906c
Yevhen Laichenkov <elaichenkov@gmail.com>
2023-09-08 23:33:30 +0300

    update(lib): omit destructuring to make it work in vite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const { join } = require('path');
-const { existsSync, readdirSync } = require('fs');
-const { randomBytes } = require('crypto');
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
 
 const addCustomCommand = () => {
   Cypress.Commands.add('verifyDownload', (fileName, options) => {
@@ -18,7 +18,7 @@ const addCustomCommand = () => {
     const { timeout, interval, contains } = { ...defaultOptions, ...options };
 
     const downloadsFolder = Cypress.config('downloadsFolder');
-    const downloadFileName = join(downloadsFolder, fileName);
+    const downloadFileName = path.join(downloadsFolder, fileName);
 
     let retries = Math.floor(timeout / interval);
 
@@ -27,7 +27,7 @@ const addCustomCommand = () => {
 
       if (retries < 1) {
         throw new Error(
-          `Failed after ${timeout} time out. \nDue to couldn't find ${fileName} file in the ${downloadsFolder} folder`
+          `Failed after ${timeout} time out. \nDue to couldn't find ${fileName} file in the ${downloadsFolder} folder`,
         );
       }
       cy.wait(interval, { log: false }).then(() => {
@@ -44,12 +44,12 @@ const addCustomCommand = () => {
           if (files !== null) {
             if (files.length > 1)
               cy.log(
-                `**WARNING!** More than one file found for the **'${fileName}'** pattern: [${files}] - the first one **[${files[0]}]** will be used`
+                `**WARNING!** More than one file found for the **'${fileName}'** pattern: [${files}] - the first one **[${files[0]}]** will be used`,
               );
 
-            const getTempName = () => `${randomBytes(8)}-temp-file-name-${randomBytes(8)}`;
+            const getTempName = () => `${crypto.randomBytes(8)}-temp-file-name-${crypto.randomBytes(8)}`;
 
-            return cy.task('isFileExist', join(downloadsFolder, files[0] || getTempName()));
+            return cy.task('isFileExist', path.join(downloadsFolder, files[0] || getTempName()));
           }
         });
       } else {
@@ -65,12 +65,12 @@ const addCustomCommand = () => {
   });
 };
 
-const isFileExist = (path) => existsSync(path);
+const isFileExist = (path) => fs.existsSync(path);
 
 const findFiles = ({ path, fileName }) => {
-  if (!existsSync(path)) return null;
+  if (!fs.existsSync(path)) return null;
 
-  return readdirSync(path).filter((file) => file.includes(fileName) && isDownloaded(file));
+  return fs.readdirSync(path).filter((file) => file.includes(fileName) && isDownloaded(file));
 };
 
 const isDownloaded = (file) => !file.endsWith('.crdownload');
