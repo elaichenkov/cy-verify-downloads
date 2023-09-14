bb861f0aed373a9bea44f8686d4d66cb36183c73
Yevhen Laichenkov <elaichenkov@gmail.com>
2023-09-14 18:24:16 +0300

    fix(#129): typescript type issue

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,19 +1,25 @@
-// load type definitions that come with Cypress module
-/// <reference types="cypress" />
-declare namespace Cypress {
-  interface Chainable {
-    /**
-     * Custom command to verify that file has been downloaded
-     * @param fileName - string
-     * @param options - { timeout: number, interval: number, contains: boolean }
-     * @example
-     *  cy.verifyDownload('filename.zip')
-     *  cy.verifyDownload('filename.zip', { timeout: 20000, interval: 500 });
-     *  cy.verifyDownload('.zip', { contains: true });
-     */
-    verifyDownload(
-      fileName: string,
-      options?: { timeout?: number; interval?: number; contains?: boolean }
-    ): Chainable<boolean>;
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to verify that file has been downloaded
+       * @param fileName - string
+       * @param options - { timeout: number, interval: number, contains: boolean }
+       * @example
+       *  cy.verifyDownload('filename.zip')
+       *  cy.verifyDownload('filename.zip', { timeout: 20000, interval: 500 });
+       *  cy.verifyDownload('.zip', { contains: true });
+       */
+      verifyDownload(
+        fileName: string,
+        options?: { timeout?: number; interval?: number; contains?: boolean },
+      ): Chainable<boolean>;
+    }
   }
 }
+
+export function addCustomCommand(): void;
+export const verifyDownloadTasks: {
+  isFileExist: (path: string) => boolean;
+  findFiles: ({ path: string, filename: string }) => string[] | null;
+};
